7859e82236b10cb7423ebde6c2285f2bfcf5435e
SuleimanKh97 <suleiman97kh@gmail.com>
2025-02-16 23:38:05 +0300

    firsttAt

---
+++ FisrstProject/FisrstProject/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+// تهيئة السلايدر
+const swiper = new Swiper('.swiper-container', {
+    loop: true, // تكرار الشرائح
+    pagination: {
+        el: '.swiper-pagination', // نقاط التصفح
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next', // زر التالي
+        prevEl: '.swiper-button-prev', // زر السابق
+    },
+    autoplay: {
+        delay: 5000, // تغيير الشريحة كل 5 ثواني
+    },
+});
