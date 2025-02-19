353ffe5bb0a540d136cbb18e0831e1b29201ffa1
SuleimanKh97 <suleiman97kh@gmail.com>
2025-02-19 23:56:19 +0300

    wedTask

---
+++ WEDTask/WEDTask/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WEDTask.Models;
+
+namespace WEDTask.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            string name = HttpContext.Session.GetString("username");
+            ViewData["name"] = name;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WEDTask/WEDTask/Controllers/UserController.cs
@@ -0,0 +1,75 @@
+﻿using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
+
+namespace WEDTask.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult LoginProsses(string email, string password)
+        {
+            string sessionEmail = HttpContext.Session.GetString("email");
+            string sessionPassword = HttpContext.Session.GetString("password");
+            if (email == sessionEmail && password == sessionPassword)
+            {
+                return RedirectToAction("Index" , "Home");
+            }
+            else
+            {
+                TempData["msg"] = "Invalid Email or Password";
+                return RedirectToAction("Login");
+            }
+
+            
+        }
+
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult RegisterProsses(string username, string email, string password, string rpassword)
+        {
+            HttpContext.Session.SetString("username", username);
+            HttpContext.Session.SetString("email", email);
+            HttpContext.Session.SetString("password", password);
+            HttpContext.Session.SetString("rpassword", rpassword);
+
+            if (password != rpassword)
+            {
+                TempData["msg"] = "Password and Confirm Password must be same";
+                return RedirectToAction("Register");
+            }
+
+            return RedirectToAction("Login");
+
+
+
+
+        }
+
+
+
+        public IActionResult Profile()
+        {
+            string name = HttpContext.Session.GetString("username");
+            string email = HttpContext.Session.GetString("email");
+            string password = HttpContext.Session.GetString("password");
+
+            ViewData["name"] = name;
+            ViewData["email"] = email;
+            ViewData["password"] = password;
+            return View();
+        }
+
+
+
+
+    }
+}
